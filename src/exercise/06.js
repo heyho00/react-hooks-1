53ea3b07d7f0ff498b01d8ce758d069a51712e4e
heyho00 <heyho9292@gmail.com>
2023-02-15 01:14:26 +0900

    6-4 Error boundary

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,6 +1,6 @@
 // useEffect: HTTP requests
-// 💯 use a status
-// http://localhost:3000/isolated/final/06.extra-2.js
+// 💯 create an ErrorBoundary component
+// http://localhost:3000/isolated/final/06.extra-4.js
 
 import * as React from 'react'
 import {
@@ -9,6 +9,21 @@ import {
   PokemonForm,
   PokemonDataView,
 } from '../pokemon'
+
+class ErrorBoundary extends React.Component {
+  state = { error: null }
+  static getDerivedStateFromError(error) {
+    return { error }
+  }
+  render() {
+    const { error } = this.state
+    if (error) {
+      return <this.props.FallbackComponent error={error} />
+    }
+
+    return this.props.children
+  }
+}
 
 function PokemonInfo({ pokemonName }) {
   const [state, setState] = React.useState({
@@ -22,13 +37,13 @@ function PokemonInfo({ pokemonName }) {
     if (!pokemonName) {
       return
     }
-    setStatus('pending')
+    setState({ status: 'pending' })
     fetchPokemon(pokemonName).then(
       pokemon => {
-        setStatus({ status: 'resolved', pokemon })
+        setState({ status: 'resolved', pokemon })
       },
       error => {
-        setStatus({ status: 'rejected', error })
+        setState({ status: 'rejected', error })
       },
     )
   }, [pokemonName])
@@ -38,17 +53,22 @@ function PokemonInfo({ pokemonName }) {
   } else if (status === 'pending') {
     return <PokemonInfoFallback name={pokemonName} />
   } else if (status === 'rejected') {
-    return (
-      <div>
-        There was an error:{' '}
-        <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
-      </div>
-    )
+    // this will be handled by an error boundary
+    throw error
   } else if (status === 'resolved') {
     return <PokemonDataView pokemon={pokemon} />
   }
 
   throw new Error('This should be impossible')
+}
+
+function ErrorFallback({ error }) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+    </div>
+  )
 }
 
 function App() {
@@ -63,30 +83,13 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
 }
 
 export default App
-
-
-// 여러 상태 업데이터를 연속으로 호출하고 있음.
-
-// 이는 일반적으로 문제가 되지 않지만 상태 업데이트 프로그램을 호출할 때마다 구성 요소가 다시 렌더링될 수 있다.
-
-// React는 일반적으로 이러한 호출을 일괄 처리하므로 단일 재렌더링만 얻을 수 있지만 비동기 콜백(Promise 성공 및 오류 핸들러)에서는 이를 수행할 수 없다.
-
-// 그래서 원래는 
-
-// setStatus('resolved')
-
-// setPokemon(pokemon)
-
-// 이런식으로 작성하게 되면 에러났었음.
-
-// React 18에서는 비동기 콜백에 대한 자동 일괄 처리도 지원하므로 더 이상 그렇지 않다.
-
-// 그래도 개별 useState 후크를 사용하여 상태를 유지하는 것보다 밀접하게 관련된 상태를 객체로 유지하는 것이 좋다.
 
